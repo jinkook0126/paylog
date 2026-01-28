9063762c3fedffa9da96ed5775a194f750e5f094
이진국 <jinkook0126@naver.com>
2026-01-28 21:55:34 +0900

    feat: hydration 에러 의심부분 수정

--- app/components/transaction/TransactionSkeleton.tsx
+++ app/components/transaction/TransactionSkeleton.tsx
@@ -1,6 +1,7 @@
 import { Skeleton } from '~/components/ui/skeleton';
 
 function TransactionSkeleton() {
+  const arr = Array.from({ length: 3 }, (_, index) => index);
   return (
     <div className="space-y-6">
       <div className="grid grid-cols-2 gap-3">
@@ -18,8 +19,8 @@ function TransactionSkeleton() {
         <Skeleton className="h-4 w-6" />
       </div>
       <div className="space-y-2">
-        {Array.from({ length: 3 }).map(() => (
-          <div key={Date.now()} className="rounded-2xl px-4 border border-border/50">
+        {arr.map((item) => (
+          <div key={item} className="rounded-2xl px-4 border border-border/50">
             <div className="flex w-fit items-center gap-4 py-4 px-2">
               <Skeleton className="size-10 shrink-0 rounded-full" />
               <div className="grid gap-2">
